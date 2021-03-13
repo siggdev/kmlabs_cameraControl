ad2e3aa8fc78c57d213002aec78c811add3888a5
Christian Bauer <development@bauer-siggenhofen.de>
2021-03-13 23:26:23 +0100

    removing debug prints

--- library/ShotController.py
+++ library/ShotController.py
@@ -1,15 +1,15 @@
 from .RedisDriver import RedisDriver
 from datetime import datetime, timedelta
 from time import sleep
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
 
 class ShotController:
     def __init__(self):
         self.redis = RedisDriver()
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setwarnings(False)
-        GPIO.setup(3, GPIO.OUT, initial=GPIO.HIGH)
+#        GPIO.setmode(GPIO.BOARD)
+#        GPIO.setwarnings(False)
+#        GPIO.setup(3, GPIO.OUT, initial=GPIO.HIGH)
 
     def get_next_shot_time(self):
         next_shot = self.redis.lrange('shot_times', 0, 0)
@@ -21,9 +21,7 @@ class ShotController:
 
     def calculate_time_until_next_shot(self):
         next_shot = self.get_next_shot_time()
-        print(next_shot.isoformat())
         current_time = datetime.now().replace(tzinfo=None).astimezone(tz=None)
-        print(current_time.isoformat())
 
         return timedelta(seconds=672)
 
@@ -37,9 +35,9 @@ class ShotController:
         return False
 
     def make_shot(self):
-        GPIO.output(3, GPIO.LOW)
+#        GPIO.output(3, GPIO.LOW)
         sleep(0.2)
-        GPIO.output(3, GPIO.HIGH)
+#        GPIO.output(3, GPIO.HIGH)
 
     def make_shot_if_due(self):
         if self.check_if_next_shot_is_due():
