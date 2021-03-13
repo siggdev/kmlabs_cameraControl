0e1f9d32a2462256b774993c8e430fb3428286b6
Christian Bauer <development@bauer-siggenhofen.de>
2021-03-13 20:15:53 +0100

    defining basic request functions

--- library/WebController.py
+++ library/WebController.py
@@ -1,8 +1,12 @@
+from flask import render_template
 
 
 class WebController:
     def __init__(self, flask_app):
         self.app = flask_app
 
-    def indexAction(self):
+    def serveIndexPage(self):
+        return render_template('index.html')
 
+    def changeSettings(self):
+        return render_template('index.html')
