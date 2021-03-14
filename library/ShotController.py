867ec023ac255f5dcba0704f5067c02a97ea6ab5
Christian Bauer <development@bauer-siggenhofen.de>
2021-03-14 21:50:33 +0100

    changing jinja2 template to match older version

--- library/ShotController.py
+++ library/ShotController.py
@@ -1,15 +1,15 @@
 from .RedisDriver import RedisDriver
 from datetime import datetime, timedelta
 from time import sleep
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 
 class ShotController:
     def __init__(self):
         self.redis = RedisDriver()
-#        GPIO.setmode(GPIO.BOARD)
-#        GPIO.setwarnings(False)
-#        GPIO.setup(3, GPIO.OUT, initial=GPIO.HIGH)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        GPIO.setup(3, GPIO.OUT, initial=GPIO.HIGH)
 
     def get_next_shot_time(self):
         next_shot = self.redis.lrange('shot_times', 0, 0)
@@ -35,9 +35,9 @@ class ShotController:
         return False
 
     def make_shot(self):
-#        GPIO.output(3, GPIO.LOW)
+        GPIO.output(3, GPIO.LOW)
         sleep(0.2)
-#        GPIO.output(3, GPIO.HIGH)
+        GPIO.output(3, GPIO.HIGH)
 
     def make_shot_if_due(self):
         if self.check_if_next_shot_is_due():
